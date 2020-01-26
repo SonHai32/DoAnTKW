7e3072c0139471346920b34b52d76fd181149d11
lamsonhai1379 <lamsonhai1379@gmail.com>
2020-01-26 23:45:05 +0700

    create banner

---
+++ script/banner.js
@@ -0,0 +1,49 @@
+const BANNERS = [
+    {URL:"./images/banner/Sale-Banner.svg" , LINK: "sale.html"},
+    {URL:"./images/banner/new-backpack-banner.svg", LINK: 'product.html?tag=BACK_PACK&id=BHBP-06' }]
+let CURRENT_INDEX = 0;
+let HREF = window.location.href;
+
+$(document).ready(function(){
+    $('.next-banner').click(function(){
+        nextBanner();
+    })
+    $('.prev-banner').click(function(){
+        prevBanner();
+    })
+    $('.banner').click(function(event){
+        if(event.target.className === 'banner'){
+            window.location.href  = `${HREF.includes('index.html') ? `./page/`+BANNERS[CURRENT_INDEX].LINK : BANNERS[CURRENT_INDEX].LINK} ` 
+        }
+    })
+})
+
+function nextBanner(){
+    if(CURRENT_INDEX >= 1){
+        CURRENT_INDEX = 0;
+    }else{
+        CURRENT_INDEX++;
+    }
+    changeBanner(CURRENT_INDEX)
+}
+
+function prevBanner(){
+    if(CURRENT_INDEX <= 0){
+        CURRENT_INDEX = BANNERS.length - 1;
+    }else{
+        CURRENT_INDEX--;
+    }
+
+    changeBanner(CURRENT_INDEX);
+}
+function changeBanner(index){
+    $(document).ready(function(){
+        $('.banner').css('background-image',`url('${HREF.includes('index.html')?BANNERS[index].URL :'.' +BANNERS[index].URL}')`);
+    })
+}
+
+changeBanner(CURRENT_INDEX);
+
+setInterval(function(){
+    nextBanner();
+},5000)
